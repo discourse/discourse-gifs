c48cf8812c3d964edde96f1d529e99c8b58facf7
Penar Musaraj <pmusaraj@gmail.com>
2023-05-11 11:51:59 -0400

    UX: Use minimasonry for layout of GIFs (#37)
    * UX: Use minimasonry for layout of GIFs
    
    * Update readme

--- javascripts/discourse/components/gif-result.js
+++ javascripts/discourse/components/gif-result.js
@@ -1,22 +1,19 @@
 import discourseComputed from "discourse-common/utils/decorators";
 import Component from "@ember/component";
+import { htmlSafe } from "@ember/template";
 
 export default Component.extend({
   tagName: "div",
   classNames: ["gif-imgwrap"],
-  classNameBindings: ["tallOne", "tallTwo"],
   attributeBindings: ["role", "tabindex"],
   role: "button",
   tabindex: 0,
 
-  @discourseComputed("gif.height", "gif.width")
-  tallOne(height, width) {
-    return height / width > 1.5;
-  },
-
-  @discourseComputed("gif.height", "gif.width")
-  tallTwo(height, width) {
-    return height / width > 1 && height / width < 1.5;
+  @discourseComputed("gif.width", "gif.height")
+  style(width, height) {
+    if (width && height) {
+      return htmlSafe(`--aspect-ratio: ${width / height};`);
+    }
   },
 
   keyDown(event) {
