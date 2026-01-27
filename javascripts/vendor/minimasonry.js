a492f26312e92edd1911d240163c2df8e5e886f6
Jarek Radosz <jarek@cvx.dev>
2026-01-27 15:48:24 +0100

    revert eslint

--- javascripts/vendor/minimasonry.js
+++ javascripts/vendor/minimasonry.js
@@ -1,5 +1,5 @@
 /** @type MiniMasonry */
-let MiniMasonry = function(conf) {
+var MiniMasonry = function(conf) {
     this._sizes             = [];
     this._columns           = [];
     this._container         = null;
@@ -30,7 +30,7 @@ let MiniMasonry = function(conf) {
 }
 
 MiniMasonry.prototype.init = function(conf) {
-    for (let i in this.conf) {
+    for (var i in this.conf) {
         if (conf[i] != undefined) {
             this.conf[i] = conf[i];
         }
@@ -42,7 +42,7 @@ MiniMasonry.prototype.init = function(conf) {
     this._currentGutterX = this.conf.gutterX;
     this._currentGutterY = this.conf.gutterY;
 
-    this._container = typeof this.conf.container === 'object' && this.conf.container.nodeName ?
+    this._container = typeof this.conf.container == 'object' && this.conf.container.nodeName ?
         this.conf.container :
         document.querySelector(this.conf.container);
 
@@ -50,7 +50,7 @@ MiniMasonry.prototype.init = function(conf) {
         throw new Error('Container not found or missing');
     }
 
-    let onResize = this.resizeThrottler.bind(this)
+    var onResize = this.resizeThrottler.bind(this)
     window.addEventListener("resize", onResize);
     this._removeListener = function() {
         window.removeEventListener("resize", onResize);
@@ -68,7 +68,7 @@ MiniMasonry.prototype.reset = function() {
     this._columns = [];
     this._count   = null;
     this._width   = this._container.clientWidth;
-    let minWidth  = this.conf.baseWidth;
+    var minWidth  = this.conf.baseWidth;
     if (this._width < minWidth) {
         this._width = minWidth;
         this._container.style.minWidth = minWidth + 'px';
@@ -96,7 +96,7 @@ MiniMasonry.prototype.getCount = function() {
 }
 
 MiniMasonry.prototype.computeWidth = function() {
-    let width;
+    var width;
     if (this.conf.surroundingGutter) {
         width = ((this._width - this._currentGutterX) / this._count) - this._currentGutterX;
     } else {
@@ -119,21 +119,21 @@ MiniMasonry.prototype.layout =  function() {
         this._count = this.getCount();
     }
     //Computing columns width
-    let colWidth = this.computeWidth();
+    var colWidth = this.computeWidth();
 
-    for (let i = 0; i < this._count; i++) {
+    for (var i = 0; i < this._count; i++) {
         this._columns[i] = 0;
     }
 
     //Saving children real heights
-    let children = this._container.children;
-    for (let k = 0;k< children.length; k++) {
+    var children = this._container.children;
+    for (var k = 0;k< children.length; k++) {
         // Set colWidth before retrieving element height if content is proportional
         children[k].style.width = colWidth + 'px';
         this._sizes[k] = children[k].clientHeight;
     }
 
-    let startX;
+    var startX;
     if (this.conf.direction == 'ltr') {
         startX = this.conf.surroundingGutter ? this._currentGutterX : 0;
     } else {
@@ -141,7 +141,7 @@ MiniMasonry.prototype.layout =  function() {
     }
     if (this._count > this._sizes.length) {
         //If more columns than children
-        let occupiedSpace = (this._sizes.length * (colWidth + this._currentGutterX)) - this._currentGutterX;
+        var occupiedSpace = (this._sizes.length * (colWidth + this._currentGutterX)) - this._currentGutterX;
         if (this.conf.wedge === false) {
             if (this.conf.direction == 'ltr') {
                 startX = ((this._width - occupiedSpace) / 2);
@@ -158,10 +158,10 @@ MiniMasonry.prototype.layout =  function() {
     }
 
     //Computing position of children
-    for (let index = 0;index < children.length; index++) {
-        let nextColumn = this.conf.minify ? this.getShortest() : this.getNextColumn(index);
+    for (var index = 0;index < children.length; index++) {
+        var nextColumn = this.conf.minify ? this.getShortest() : this.getNextColumn(index);
 
-        let childrenGutter = 0;
+        var childrenGutter = 0;
         if (this.conf.surroundingGutter || nextColumn != this._columns.length) {
             childrenGutter = this._currentGutterX;
         }
@@ -171,7 +171,7 @@ MiniMasonry.prototype.layout =  function() {
         } else {
             x = startX - ((colWidth + childrenGutter) * (nextColumn)) - colWidth;
         }
-        let y = this._columns[nextColumn];
+        var y = this._columns[nextColumn];
 
 
         children[index].style.transform = 'translate3d(' + Math.round(x) + 'px,' + Math.round(y) + 'px,0)';
@@ -187,8 +187,8 @@ MiniMasonry.prototype.getNextColumn = function(index) {
 };
 
 MiniMasonry.prototype.getShortest = function() {
-    let shortest = 0;
-    for (let i = 0; i < this._count; i++) {
+    var shortest = 0;
+    for (var i = 0; i < this._count; i++) {
         if (this._columns[i] < this._columns[shortest]) {
             shortest = i;
         }
@@ -198,8 +198,8 @@ MiniMasonry.prototype.getShortest = function() {
 };
 
 MiniMasonry.prototype.getLongest = function() {
-    let longest = 0;
-    for (let i = 0; i < this._count; i++) {
+    var longest = 0;
+    for (var i = 0; i < this._count; i++) {
         if (this._columns[i] > this._columns[longest]) {
             longest = i;
         }
@@ -224,12 +224,12 @@ MiniMasonry.prototype.resizeThrottler = function() {
 }
 
 MiniMasonry.prototype.destroy = function() {
-    if (typeof this._removeListener === "function") {
+    if (typeof this._removeListener == "function") {
         this._removeListener();
     }
 
-    let children = this._container.children;
-    for (let k = 0;k< children.length; k++) {
+    var children = this._container.children;
+    for (var k = 0;k< children.length; k++) {
         children[k].style.removeProperty('width');
         children[k].style.removeProperty('transform');
     }
